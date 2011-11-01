2248e9366e81edda08b4b076cc45d39b0e401961
Ryan Alexander <scloopy@gmail.com>
2011-11-01 16:14:07 -0700

    Initial import

---
+++ test.js
@@ -0,0 +1,12 @@
+var plask = require('plask');
+var stats = require('./stats');
+
+plask.simpleWindow({
+    init: function(){
+        this.framerate(60);
+        this.stats = new stats.Stats(100, 60).open();
+    },
+    draw: function(){
+        this.stats.update();
+    }
+});
